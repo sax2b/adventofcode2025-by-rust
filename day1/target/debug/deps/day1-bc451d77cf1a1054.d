8a72a6605ccca15e20987bf8c7ff1f8b0602f55e
setthawutleela <setthawut.leela@gmail.com>
2025-12-03 14:21:02 +0700

    feat: add gitignore

--- day1/target/debug/deps/day1-bc451d77cf1a1054.d
+++
@@ -1,5 +0,0 @@
-/Users/saab/adventofcode2025-by-rust/day1/target/debug/deps/day1-bc451d77cf1a1054.d: src/main.rs
-
-/Users/saab/adventofcode2025-by-rust/day1/target/debug/deps/day1-bc451d77cf1a1054: src/main.rs
-
-src/main.rs:
